efc3c42137b6e42ca6764113d740925def67cd3b
hjy <1441211576@qq.com>
2024-05-15 14:46:12 +0800

    fix: fix type bug

--- packages/data-structure/src/utils/resolve.ts
+++ packages/data-structure/src/utils/resolve.ts
@@ -2,14 +2,14 @@
  * @Author: hjy 1441211576@qq.com
  * @Date: 2024-05-15 09:20:17
  * @LastEditors: hjy 1441211576@qq.com
- * @LastEditTime: 2024-05-15 10:07:06
+ * @LastEditTime: 2024-05-15 14:36:29
  * @FilePath: /algorithm-visualization/packages/data-structure/src/utils/resolve.ts
  * @Description: resolve data
  */
 
 interface IPros {
   type: string
-  actions: Array<{ op: string; value?: number }>
+  actions: Array<{ op: string; value?: Object }>
 }
 const parseSchema2VChart = (data: any[], schema: IPros, curAction: number = -1) => {
   const { actions } = schema
