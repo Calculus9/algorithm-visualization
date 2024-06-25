27d36c16dbaa3296df3fba4e77b205425924d926
hh <1441211576@qq.com>
2024-06-25 19:18:34 +0800

    feat：import Monaco Editor

--- packages/editor/index.tsx
+++ packages/editor/index.tsx
@@ -1,12 +1,44 @@
-import React from "react"
+/*
+ * @Author: hh 1441211576@qq.com
+ * @Date: 2024-06-25 14:56:42
+ * @LastEditors: hh 1441211576@qq.com
+ * @LastEditTime: 2024-06-25 19:18:27
+ * @FilePath: \algorithm-visualization\packages\editor\index.tsx
+ * @Description: 
+ * 
+ */
+import React, { useCallback, useState } from "react"
+import { Editor, OnChange } from '@monaco-editor/react';
+import { Space, Button } from "@arco-design/web-react";
+import { IconPlayArrow } from '@arco-design/web-react/icon'
+// interface IProps {
+//   text: string
+// }
+const MonacoEditor: React.FC = () => {
+  const [code, setCode] = useState("");
 
+  const handleEditorChange: OnChange = (value, event) => {
+    setCode(value ?? '');
+  };
 
-interface IProps {
-  text: string
+  const handleButtonClick = useCallback(() => {
+    // 调用vchart
+  }, [code])
+
+  return (
+    <div style={{ height: '500px' }}>
+      <Space>
+      <Button type='outline' icon={<IconPlayArrow/>} shape="circle" onClick={handleButtonClick} />
+      </Space>
+      <Editor
+        height="500px"
+        language="javascript"
+        theme="vs-light"
+        value={code}
+        onChange={handleEditorChange}
+      />
+    </div>
+  );
 }
-const Editor = (props: IProps) => {
-  const { text } = props
-  return <div>{text}</div>
-}
 
-export default Editor
+export default MonacoEditor
