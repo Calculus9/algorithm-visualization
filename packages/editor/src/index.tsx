ce271403231f97c175a1e0cfd0347b64caa5bf53
hh <1441211576@qq.com>
2024-07-18 20:20:22 +0800

    feat: add the monaco code remind

--- packages/editor/src/index.tsx
+++ packages/editor/src/index.tsx
@@ -2,19 +2,71 @@
  * @Author: hh 1441211576@qq.com
  * @Date: 2024-06-25 14:56:42
  * @LastEditors: hh 1441211576@qq.com
- * @LastEditTime: 2024-07-13 11:20:15
+ * @LastEditTime: 2024-07-18 20:18:37
  * @FilePath: \algorithm-visualization\packages\editor\src\index.tsx
  * @Description:
  *
  */
-import React, { useCallback, useRef, useState } from 'react'
-import { Editor } from '@monaco-editor/react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+import { Editor, useMonaco } from '@monaco-editor/react'
 import { Space, Button } from '@arco-design/web-react'
 import { IconPlayArrow } from '@arco-design/web-react/icon'
 
 const MonacoEditor: React.FC = ({ onChange }) => {
   const [code, setCode] = useState(localStorage.getItem('storage') || '')
   const editorRef = useRef(null)
+  const [editorInstance, setEditorInstance] = useState(null)
+  const monaco = useMonaco()
+
+  const onMount = useCallback(editor => {
+    editorRef.current = editor
+    setEditorInstance(editor)
+  }, [])
+
+  useEffect(() => {
+    if (editorInstance) {
+      const dispose = monaco?.languages.registerCompletionItemProvider('javascript', {
+        provideCompletionItems: function (model, position, context, token) {
+          const word = model.getWordUntilPosition(position)
+          const range = {
+            startLineNumber: position.lineNumber,
+            endLineNumber: position.lineNumber,
+            startColumn: word.startColumn,
+            endColumn: word.endColumn
+          }
+          const suggestions = [
+            {
+              label: 'VisArray',
+              kind: monaco.languages.CompletionItemKind.Class,
+              insertText: 'VisArray()',
+              range: range
+            },
+            {
+              label: 'Alvis',
+              kind: monaco.languages.CompletionItemKind.Class,
+              insertText: `Alvis("", { data: [], options: {}})`,
+              range: range
+            }
+          ]
+          const str = model.getValue()
+          const arrayMethods = ['push', 'pop', 'insert', 'get', 'set', 'delete']
+          if (str.includes('VisArray' || 'Alvis')) {
+            arrayMethods?.map(d => {
+              suggestions.push({
+                label: d,
+                kind: monaco.languages.CompletionItemKind.Method,
+                insertText: `${d}()`,
+                range: range
+              })
+            })
+          }
+
+          return { suggestions: suggestions }
+        }
+      })
+      return () => dispose?.dispose()
+    }
+  }, [editorInstance])
 
   const handleButtonClick = useCallback(() => {
     const currentCode = editorRef?.current?.getValue()
@@ -22,7 +74,7 @@ const MonacoEditor: React.FC = ({ onChange }) => {
   }, [code])
 
   return (
-    <div style={{ height: '500px' }}>
+    <div id={'container'} style={{ height: '500px' }}>
       <Space>
         <Button
           type='outline'
@@ -32,13 +84,12 @@ const MonacoEditor: React.FC = ({ onChange }) => {
         />
       </Space>
       <Editor
-        onMount={editor => {
-          editorRef.current = editor
-        }}
+        onMount={onMount}
         height='500px'
         language='javascript'
         theme='vs-light'
         value={code}
+        // options={}
         // onChange={handleEditorChange}
       />
     </div>
