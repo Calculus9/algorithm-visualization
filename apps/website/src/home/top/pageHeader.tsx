73ac955c72bb6e0d05b660793c1c2443216c9deb
hh <1441211576@qq.com>
2024-08-09 09:37:21 +0800

    feat: i18n add success

--- apps/website/src/home/top/pageHeader.tsx
+++ apps/website/src/home/top/pageHeader.tsx
@@ -1,22 +1,23 @@
 /*
  * @Author: hh 1441211576@qq.com
  * @Date: 2024-08-06 16:12:25
- * @LastEditors: hjy 1441211576@qq.com
- * @LastEditTime: 2024-08-08 22:58:34
+ * @LastEditors: hh 1441211576@qq.com
+ * @LastEditTime: 2024-08-09 09:36:34
  * @FilePath: \algorithm-visualization\apps\website\src\home\top\pageHeader.tsx
  * @Description:
  *
  */
-import { PageHeader, Menu } from '@arco-design/web-react'
+import { PageHeader, Menu, Link, Dropdown } from '@arco-design/web-react'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import { useThemeStore, usePageStore, useLngStore } from '../../store/create'
 const MenuItem = Menu.Item
-import { ZhIMG, ENIMG } from '../../constant.tsx'
+import { ZhIMG, GITHUBICON } from '../../constant.tsx'
+import { LANGUAGES } from './constants.ts'
 
 const MenuRoute = () => {
-  const { t } = useTranslation()
+  const { t, i18n } = useTranslation()
   const menuList = t('menuList', { returnObjects: true })
 
   const themeColor = useThemeStore(state => state.themeColor)
@@ -25,8 +26,22 @@ const MenuRoute = () => {
   const setPage = usePageStore(state => state.setPage)
   const navigate = useNavigate()
 
-  const lng = useLngStore(state => state.lng)
-  const setLng = useLngStore(state => state.setLng)
+  const droplist = (
+    <Menu>
+      {LANGUAGES?.map((item: { key: string; value: string }) => {
+        return (
+          <Menu.Item
+            onClick={() => {
+              i18n.changeLanguage((i18n.language = item?.value))
+            }}
+            key={item?.value}
+          >
+            {item?.key}
+          </Menu.Item>
+        )
+      })}
+    </Menu>
+  )
 
   return (
     <div style={{ background: 'var(--color-fill-2)' }}>
@@ -59,7 +74,16 @@ const MenuRoute = () => {
           </Menu>
         }
         extra={
-          <div onClick={() => setLng(!lng as unknown as number)}>{lng ? <ZhIMG /> : <ENIMG />}</div>
+          <>
+            <Dropdown droplist={droplist} position='bl'>
+              <span>{<ZhIMG />}</span>
+            </Dropdown>
+            <span style={{ marginLeft: 10 }}>
+              <Link href='https://github.com/Calculus9/algorithm-visualization'>
+                {<GITHUBICON />}
+              </Link>
+            </span>
+          </>
         }
       />
     </div>
